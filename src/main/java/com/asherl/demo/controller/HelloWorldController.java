4bf00c9e41055465eadab2ae259257f0b3070f42
asher-lab <asher01000001@gmail.com>
2023-02-16 16:39:28 +0800

    rename files

--- src/main/java/com/asherl/demo/controller/HelloWorldController.java
+++ src/main/java/com/asherl/demo/controller/HelloWorldController.java
@@ -9,6 +9,6 @@ public class HelloWorldController {
     @GetMapping("/")
     public String sayHello() {
         return "<span style='color:" + "blue"
-                + "; font-size: 64px; text-align: center; display: flex; justify-content: center; align-items: center; height: 100vh;'>Hello World</span>";
+                + "; font-size: 72px; text-align: center; display: flex; justify-content: center; align-items: center; height: 100vh;'>Hello World</span>";
     }
 }
